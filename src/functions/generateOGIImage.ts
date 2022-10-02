a271ffabcaa2192e4a4b077d9a24e2a40a20fc2a
ozzyee <oearle10@gmail.com>
2022-10-02 02:40:26 +0100

    fix url

--- src/functions/generateOGIImage.ts
+++ src/functions/generateOGIImage.ts
@@ -1,12 +1,12 @@
 import puppeteer from "puppeteer";
 
-export const generateOGIImage = async () => {
+export const generateOGIImage = async (_url: string) => {
   try {
     const browser = await puppeteer.launch({
       args: ['--no-sandbox']
     });
     const page = await browser.newPage();
-    await page.goto("http://localhost:3000");
+    await page.goto(_url);
     await page.setViewport({ width: 2048, height: 1170 });
     const image = await page.screenshot();
 
--- src/pages/api/og-img.ts
+++ src/pages/api/og-img.ts
@@ -9,7 +9,10 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const file = await generateOGIImage()
+  const url = req.headers.host
+  const _url = url?.includes('localhost') ? `http://${url}/` : `https://${url}`;
+
+  const file = await generateOGIImage(_url)
   res.setHeader('Content-Type', `image/png`);
   res.end(file);
 }
